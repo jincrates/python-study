74bb8ef0b8161c92cfa2bbd9b27a4655c4f87908
jincrates <94jingyu@gmail.com>
2021-09-12 23:41:15 +0900

    function2

--- 04_함수/function1.py
+++ 04_함수/function1.py
@@ -2,6 +2,14 @@
 def print_sqrt():
     r1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
     r2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+    
+    print('해는 {} 또는 {}'.format(r1, r2))
+
+
+#5
+def print_root(a, b, c): #매개변수(parameter): 정의해서 사용하는 이름
+    r1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+    r2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
 
     print('해는 {} 또는 {}'.format(r1, r2))
 
@@ -36,3 +44,20 @@ a = 2
 b = -6
 c = -8
 print_sqrt()
+
+
+#6
+x = 2
+y = -6
+z = -8
+print_root(x, y, z)  #실행인자(argument): 실행할 때 넘기는 값
+
+
+#7
+def print_round(number):
+    rounded = round(number)
+    print(rounded)
+
+print_round(4.6)
+print_round(2.2)
+print_round(3.7)
